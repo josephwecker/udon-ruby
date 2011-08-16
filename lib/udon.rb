4143f9c9276e8756beb5ecae183f87aeea65d799
Joseph Wecker <joseph.wecker@gmail.com>
2011-08-16 14:36:32 -0700

    Version constant for introspecting etc.

--- lib/udon.rb
+++ lib/udon.rb
@@ -1,3 +1,6 @@
 module Udon
-
+  VERSION = File.exist?(File.join(File.dirname(__FILE__),'VERSION')) ? File.read(File.join(File.dirname(__FILE__),'VERSION')) : ""
+  class << self
+    def version() VERSION end
+  end
 end
