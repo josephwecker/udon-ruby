cc8115dfa524da1ece6276dcbad2b0b854bd331b
Joseph Wecker <joseph.wecker@gmail.com>
2011-08-17 20:30:04 -0700

    Hook in parser and start printing the AST to help while constructing

--- lib/udon.rb
+++ lib/udon.rb
@@ -1,6 +1,16 @@
 module Udon
+  require 'udon/udon_parser'
   VERSION = File.exist?(File.join(File.dirname(__FILE__),'VERSION')) ? File.read(File.join(File.dirname(__FILE__),'VERSION')) : ""
   class << self
     def version() VERSION end
+
+    def parse(source, opts={})
+      res = UdonParser::Parser.new(source, opts).parse
+      require 'pp'
+      puts "\n----------------- AST ---------------------"
+      pp res
+      puts "\n-------------------------------------------"
+      return res
+    end
   end
 end
