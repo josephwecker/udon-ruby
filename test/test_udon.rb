eeb495339403641f33135e425937909ca109812e
Joseph Wecker <jwecker@justin.tv>
2011-08-19 13:12:27 -0700

    Formatting and test that comment correctly retains newlines and resets to parent at the correct position

--- test/test_udon.rb
+++ test/test_udon.rb
@@ -3,10 +3,16 @@ $KCODE='U'
 
 class TestUdon < MiniTest::Unit::TestCase
   def test_blank_documents
-    assert_equal(''.udon,[])
+    ##############
+    assert_equal         [],                    ''.udon
+    ##############
+
     (0..3).each do
       s = randstr(200,"      \t\n\r")
-      assert_equal(s,s.udon.join(''))
+
+      ##############
+      assert_equal       s,                     s.udon.join('')
+      ##############
     end
   end
 
@@ -22,25 +28,39 @@ class TestUdon < MiniTest::Unit::TestCase
     (0..3).each do
       s = randstr(100,chars)
       s.gsub! /^\s*(<\||#|\|)/u, '' # Remove stuff that triggers udon mode
-      assert_equal(s,s.udon.join(''))
+
+      ##############
+      assert_equal       s,                     s.udon.join('')
+      ##############
     end
   end
 
   def test_block_comment_indent_level
     leading = randstr(200,"      \t\n\r") + "\n"
     comment = <<-COMMENT
-      #  line 1
-         line 2
-        line 3
+      #  line 0
+         line 1
+        line 2
+
        line 4
+
+      # comment 2
      COMMENT
     following = randstr(200,"     \t\n\r")
     s = (leading + comment + following)
     r = s.udon
-    assert_instance_of(UdonParser::UNode, r.last)
-    assert_equal('line 1', r.last.c[0])
-    assert_equal('line 2', r.last.c[1])
-    assert_equal('line 3', r.last.c[2])
-    assert_equal('line 4', r.last.c[3])
+    lines = r[-2].c
+
+    ##############
+    assert_instance_of   UdonParser::UNode,     r[-2]
+    assert_equal         'comment',             r[-2].name
+    assert_equal         'line 0',              lines[0]
+    assert_equal         'line 1',              lines[1]
+    assert_equal         'line 2',              lines[2]
+    assert_equal         "\n",                  lines[3]
+    assert_equal         'line 4',              lines[4]
+    assert_instance_of   UdonParser::UNode,     r[-1]
+    assert_equal         'comment',             r[-1].name
+    ##############
   end
 end
